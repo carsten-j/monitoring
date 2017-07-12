fd1a0967112c32821d03e4efad0eaa995d002204
carstenj <carstenj@gmail.com>
2017-07-12 20:19:25 +0200

    Final version for blog post

--- grafana/entrypoint.sh
+++ grafana/entrypoint.sh
@@ -1,35 +1,37 @@
 #!/usr/bin/env sh
 
 transform_dashboard_json() {
-	cat $1 | jq --arg INFLUX_DATABASE "$INFLUX_DATABASE" '.rows[0].panels[0].datasource = $INFLUX_DATABASE | { dashboard:.,overwrite: true }'
+    cat $1 | jq --arg INFLUX_DATABASE "$INFLUX_DATABASE" '.rows[0].panels[0].datasource = $INFLUX_DATABASE | { dashboard:. ,overwrite: true }'
 }
 
 if [ ! -f "/var/lib/grafana/.db" ]; then
 
-	url="http://admin:admin@localhost:3000/"
+    url="http://${GRAFANA_USER}:${GRAFANA_PASSWORD}@localhost:3000/"
 
-	exec /run.sh $@ &
+    exec /run.sh $@ &
 
-	until curl -s ${url}"api/datasources" 2>/dev/null; do
-		sleep 1
-	done
+    # consider exiting to avoid infinite loop
+    until curl -s ${url}"api/datasources" 2>/dev/null; do
+        sleep 2
+    done
 
-	for datasource in /etc/grafana/datasources/*.json; do
-		curl "${url}api/datasources" \
-			-X POST -H 'Content-Type: application/json' \
-			-d "$(envsubst <$datasource)"
-	done
+    # try with -u for user:password
+    for datasource in /etc/grafana/datasources/*.json; do
+        curl "${url}api/datasources" \
+            POST -H 'Content-Type: application/json' \
+            --data "$(envsubst <$datasource)"
+    done
 
-	for dashboard in /etc/grafana/dashboards/*.json; do
-		dashboard_json=$(transform_dashboard_json $dashboard)
-		curl "${url}api/dashboards/db" \
-			-X POST -H 'Content-Type: application/json' \
-			--data "$dashboard_json"
-	done
+    for dashboard in /etc/grafana/dashboards/*.json; do
+        dashboard_json=$(transform_dashboard_json $dashboard)
+        curl "${url}api/dashboards/db" \
+            POST -H 'Content-Type: application/json' \
+            --data "$dashboard_json"
+    done
 
-	touch "/var/lib/grafana/.db"
+    touch "/var/lib/grafana/.db"
 
-	kill $(pgrep grafana)
+    kill $(pgrep grafana)
 
 fi
 
--- influxdb/entrypoint.sh
+++ influxdb/entrypoint.sh
@@ -1,32 +1,23 @@
 #!/usr/bin/env sh
-#set -e
 
 if [ ! -f "/var/lib/influxdb/.db" ]; then
-	exec influxd &
+    exec influxd &
 
-	#LOOP_MAX_COUNT=12
-	#LOOP_INCREMENT_COUNT=1
-	#LOOP_COUNT_SLEEP=5
+    until wget "http://localhost:8086/ping" 2>/dev/null; do
+        sleep 2
+    done
 
-	until wget "http://localhost:8086/ping" 2>/dev/null; do
-	#	if ($LOOP_INCREMENT_COUNT >= $LOOP_MAX_COUNT); then
-	#		break
-	#	fi
-	#	LOOP_INCREMENT_COUNT+=1
-		sleep 2 # $LOOP_COUNT_SLEEP
-	done
+    influx -host=localhost -port=8086 \
+        -execute="CREATE DATABASE ${INFLUX_DATABASE}"
 
-	influx -host=localhost -port=8086 \
-		-execute="CREATE DATABASE ${INFLUX_DATABASE}"
+    influx -host=localhost -port=8086 \
+        -execute="CREATE USER ${INFLUX_USER}" \
+        "WITH PASSWORD ${INFLUX_PASSWORD}" \
+        "WITH ALL PRIVILEGES"
 
-	influx -host=localhost -port=8086 \
-		-execute="CREATE USER ${INFLUX_USER} " \
-		"WITH PASSWORD ${INFLUX_PASSWORD} " \
-		"WITH ALL PRIVILEGES"
+    touch "/var/lib/influxdb/.db"
 
-	touch "/var/lib/influxdb/.db"
-
-	kill -s TERM %1
+    kill -s TERM %1
 fi
 
 exec influxd
